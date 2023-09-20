cf473fcf1bf33d8da432c1e3be799680249a514d
piyushinduja <74929365+piyushinduja@users.noreply.github.com>
2023-09-20 12:44:09 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=8:30:00
+#SBATCH --time=6:45:00
 #SBATCH --mem=2GB
 #SBATCH --mail-user=u1468310@utah.edu
 #SBATCH --mail-type=FAIL,END
